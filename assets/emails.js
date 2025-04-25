e0780affa2e040ba511e615cc48290df854ac345
pwr-performanceanalysis <122685023+pwr-performanceanalysis@users.noreply.github.com>
2025-04-25 13:20:28 +0200

    Create emails.js

---
+++ assets/emails.js
@@ -0,0 +1,10 @@
+// emails.js
+// Definerer listen med obfuskerte e-postadresser
+
+const allowedObfuscatedEmails = [
+  "tromaneromanosromantroman@romaneromanoxromanaromanmromanpromanlromaneroman.romancomanoromanmroman",
+  "bromanrromanuromankromaneromanrroman@romandromanommanaromanenromaneroman.romanromanomanoroman",
+  "mromanaromanromanumaneromanlroman@romanaromanlromanpromanharomantromaneromankroman.romanaromanriroman"
+];
+
+// Ingen annen kode er nødvendig her.
