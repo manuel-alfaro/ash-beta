81d85dd78f7754de30295a0cb40785d0a1c4b0f4
pwr-performanceanalysis <122685023+pwr-performanceanalysis@users.noreply.github.com>
2025-04-29 10:54:43 +0200

    Update emails.js

--- assets/emails.js
+++ assets/emails.js
@@ -2,7 +2,11 @@
 // Definerer listen med obfuskerte e-postadresser
 
 const allowedObfuscatedEmails = [
-  "manuel@alphatek.ai"
+  "manuel@alphatek.ai",
+  "stighaugsbo.andersson@volvat.no",
+  "ynghav@gmail.com",
+  "yngve.havn@volvat.no"
+  
 ];
 
 // Ingen annen kode er nødvendig her.
